8f88c8abc5cc84b4f4d7af0a8268611ac70ca996
Clement Neo <clement.neohx@gmail.com>
2021-06-22 16:17:27 +0800

    removed space in UNIQUE constraint

--- dbhelper_postgresql.py
+++ dbhelper_postgresql.py
@@ -45,7 +45,7 @@ class DBHelper:
                 "group_id integer NOT NULL," \
                 "offset_in_seconds integer NOT NULL," \
                 "FOREIGN KEY (group_id) REFERENCES users (group_id)" \
-                "UNIQUE (group_id))"
+                "UNIQUE(group_id))"
         cursor = self.conn.cursor()
         cursor.execute(stmt1)
         cursor.execute(stmt2)
